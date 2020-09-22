da22c2a7d91636a7f446f06ec6ed8d539528388d
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-09-22 04:33:04 -0700

    Created Usercontroller class

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,28 @@
 class UserController < ApplicationController
 
-   get '/users/signup' do
+
+   #user can create an account
+   get '/signup' do
     #binding.pry 
     erb :'users/signup'
    end
 
 
-   post '/users/signup' do
+   post '/signup' do
     
-    @user = User.create(username: params[:username], password: params[:password])
-    redirect "/users/#{@user.id}"
+    user = User.create(username: params[:username], password: params[:password])
+    if user.save
+      session[user_id]= user.id
+      redirect '/linetrackers'
+
+    elsif User.find(username: params[:username])
+      @error ="Account with that Username already exits!"
+      erb :'/users/signup' 
+
+    else
+      @error = "Username and password can not be blank."
+      erb :'/users/signup' 
+    end 
 
    end
 
@@ -20,4 +33,6 @@ class UserController < ApplicationController
     erb :'users/show'
     
    end
+
+   #user can delete an account
 end
